1491fbfc5b5ddcd720b32ae9516b7f0c527909f7
EzeLRA <ezeleocraft@gmail.com>
2025-07-27 18:12:37 -0300

    implementacion receta

--- src/main/java/com/empanadas/EmpanadasModDataGenerator.java
+++ src/main/java/com/empanadas/EmpanadasModDataGenerator.java
@@ -6,6 +6,8 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 public class EmpanadasModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		//Paquete principal para agregar los proveedores
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+		pack.addProvider(EmpanadasModRecipeProvider::new);
 	}
 }
---
+++ src/main/java/com/empanadas/EmpanadasModRecipeProvider.java
@@ -0,0 +1,46 @@
+package com.empanadas;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import com.empanadas.Items.ModItems;
+import net.minecraft.data.recipe.RecipeExporter;
+import net.minecraft.data.recipe.RecipeGenerator;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.ItemTags;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+
+public class EmpanadasModRecipeProvider extends FabricRecipeProvider {
+    public EmpanadasModRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected RecipeGenerator getRecipeGenerator(RegistryWrapper.WrapperLookup registryLookup, RecipeExporter exporter) {
+        return new RecipeGenerator(registryLookup, exporter) {
+            @Override
+            public void generate() {
+                RegistryWrapper.Impl<Item> itemLookup = registries.getOrThrow(RegistryKeys.ITEM);
+                //Receta para Tapa de Empanadas
+                createShapeless(RecipeCategory.FOOD, ModItems.TAPA_EMPANADA).input(Items.EGG).input(Items.WHEAT).input(Items.POTION)
+                        .criterion(hasItem(Items.EGG),conditionsFromItem(Items.EGG))
+                        .criterion(hasItem(Items.WHEAT),conditionsFromItem(Items.WHEAT))
+                        .criterion(hasItem(Items.POTION),conditionsFromItem(Items.POTION))
+                        .offerTo(exporter);
+
+            }
+        };
+    }
+
+    @Override
+    public String getName() {
+        return "FabricDocsReferenceRecipeProvider";
+    }
+}
